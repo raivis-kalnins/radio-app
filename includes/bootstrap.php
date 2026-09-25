69824ef36b5b3b61d7bfa230c7ac49d87b9e049e
Raivis Kalnins <raivis@63.lv>
2026-09-25 15:25:43 +0100

    upgrade

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-const GO_APP_VERSION = '1.1.0';
+const GO_APP_VERSION = '1.3.3';
 
 function go_supported_languages(): array
 {
--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var VERSION = '1.3.1';
+var VERSION = '1.3.3';
 var STATIC_CACHE = 'go-app-static-' + VERSION;
 var TILE_CACHE = 'go-app-map-tiles-' + VERSION;
 var DATA_CACHE = 'go-app-data-' + VERSION;
